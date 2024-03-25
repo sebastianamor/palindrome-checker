bda24e4f0e58b08fdf3e168feb69bfdea9674f06
¨Jane <smoreira11@hotmail.com>
2024-03-25 22:46:00 +0900

    steel java

--- script.js
+++ script.js
@@ -2,8 +2,28 @@ const palindromeBtn = document.getElementById("check-btn")
 const inputText = document.getElementById("text-input")
 const resultInput = document.getElementById("result")
 
-palindromeBtn.addEventListener("click", ()=> {
-    if (inputText.value == ""){
-        alert('Please input value');
-    }
-})
+function cleanInputString(srt) {
+    const regex = /[+-\s]/g;
+    return srt.replace(regex,"");
+}
+const paliCheck = (input) => {
+    let cleanInput = cleanInputString(input); 
+   let  casedInput = cleanInput.toLowerCase();
+   let arrayedInput = casedInput.split("");
+   let reversedArray = arrayedInput.reverse();
+
+   if (arrayedInput === reversedArray){
+    return true ;
+   } else {
+    return false ; 
+   }
+};
+
+palindromeBtn?.addEventListener( 'click', () => {
+    if (inputText.value == "") {
+        alert("pleace input a value");
+    }else if (paliCheck(inputText.value)){
+     resultInput.innerText = `${inputText.value} is a palindrome ` 
+    } else { resultInput.innerText = `${inputText.value} is not a palindrome ` 
+    };
+});
