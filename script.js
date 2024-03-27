066859b1ad32780a113f24037e37cc1707340e3d
¨Jane <smoreira11@hotmail.com>
2024-03-27 22:10:10 +0900

    出来た！！！

--- script.js
+++ script.js
@@ -3,25 +3,38 @@ const iText = document.getElementById("text-input")
 const resultInput = document.getElementById("result")
 
 function cleanInputString(srt) {
-    const regex = /[+-\s]/g;
+    const regex = /[_,():\/;\.{}+-\s]/g;
     return srt.replace(regex,'');
+};
+
+function arraysAreEqual(array1,array2){
+    if (array1.length != array2.length) {
+        return false;
+    }
+    for (let i = 0; i< array1.length; i++) {
+        if (array1[i] !== array2[i]){
+            return false;
+        }
+    } 
+    return true;
 }
+
 const paliCheck = (input) => {
-    let cleanInput = cleanInputString(input); 
+   let cleanInput = cleanInputString(input); 
    let  casedInput = cleanInput.toLowerCase();
    let arrayedInput = casedInput.split("");
-   let reversedArray = arrayedInput.reverse();
+   let reversedArray = arrayedInput.toReversed();
 
-   if (arrayedInput === reversedArray ){
-    return true ;
+   if (arraysAreEqual(arrayedInput,reversedArray)){
+    return true;
    } else {
-    return false ; 
+    return false;
    }
-}; 
+   }
 
-palindromeBtn?.addEventListener('click', () => {
+palindromeBtn?.addEventListener('click', ()=>{
     if (iText.value == "") {
-        alert("pleace input a value");
+        alert("please input a value");
     }else if (paliCheck(iText.value)){
      resultInput.innerText = `${iText.value} is a palindrome ` 
     } else { resultInput.innerText = `${iText.value} is not a palindrome ` 
